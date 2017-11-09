0c12f2f95b11f2d92203ec0f543a5cebe20c232d
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 02:18:18 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    end_with? '.'
   end
 
   def question?
-
+    end_with? '?'
   end
 
   def exclamation?
-
+    end_with? '!'
   end
 
   def count_sentences
-
+    split(/[.?!]+/).size
   end
 end
